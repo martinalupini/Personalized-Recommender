99ac839b1efef41da18550f4510364ed6982a853
Martina <martinalupini11@gmail.com>
2025-04-10 13:55:44 +0100

    finished

--- Utils.py
+++ Utils.py
@@ -1,14 +1,14 @@
 def write_top_k_neighbours(top_k_neigh, f):
-    i = 0
+    i = 1
     for neighbours in top_k_neigh:
-        f.write(str(i) + ") id: " + neighbours + ", sim: " + str(top_k_neigh[neighbours]) + '\n')
+        f.write(str(i) + ") id: " + neighbours + ", sim: " + str(round(top_k_neigh[neighbours], 6)) + '\n')
         i += 1
 
 
 def write_top_k_most_similar(top_k_neigh, movie_dict, f):
-    i = 0
+    i = 1
     for item in top_k_neigh:
-        f.write(str(i) + ") id: " + item + ", title: " + movie_dict[item]["Title"] + ", sim: " + str(top_k_neigh[item]) + '\n')
+        f.write(str(i) + ") id: " + item + ", title: " + movie_dict[item]["Title"] + ", sim: " + str(round(top_k_neigh[item], 6)) + '\n')
         i += 1
 
 
@@ -16,6 +16,6 @@ def print_recommendations(rating_list, movie_dict, file):
     index = 1
     for movie in rating_list:
         file.write(str(index) + ") ID: " + movie + " Title: " + movie_dict[movie]["Title"] +
-                   "\nRating prediction: " + str(rating_list[movie]) + "\n**********************************\n")
+                   "\nRating prediction: " + str(round(rating_list[movie], 6)) + "\n**********************************\n")
         index += 1
 
